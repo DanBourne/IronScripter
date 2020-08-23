1f5e2c09b7f4146a8f61165dca22abc3fbbbfe62
Dan-B-92 <danbourne1992@gmail.com>
2020-08-23 17:41:38 +0100

    Commit
    Complete

--- Beginner/PowerShell Beginners Have to Start Somewhere.ps1
+++ Beginner/PowerShell Beginners Have to Start Somewhere.ps1
@@ -1,7 +1,11 @@
 <#
+PowerShell Beginners Have to Start Somewhere - July 15, 2019
+
 Get all files in a given folder including subfolders and display a result that shows the total number of files, 
 the total size of all files, the average file size, the computer name, and the date when you ran the command.
+
 #>
+
 
 #Get file information into the $Files variable
 $Files = Get-ChildItem C:\Temp -Recurse -File
